50d252d397a5cc2c0622ec311a6b5b690560a079
mc303 <daniel@buonocore.nl>
2023-12-13 14:33:06 +0100

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,22 +1,8 @@
-
+#!/usr/bin/env bash
 
 # DOCKER_REPO="ghcr.io/mc303/hass-node-red:latest"
 # BUIILDX_REPO='buildhassnodered'
 # BUILD_PLATFORM="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
-
-# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-# #create multiplatform environment
-# docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
-# docker buildx use ${BUIILDX_REPO}
-
-# #build multiplatform docker image
-# docker buildx build --platform=${BUILD_PLATFORM} -t ${DOCKER_REPO} --push .
-
-# #remove multiplatform environment
-# docker buildx rm ${BUIILDX_REPO}
-
-#!/usr/bin/env bash
 
 DOCKER_USER_REPO=node-red/node-red 
 DOCKER_API_URL=https://api.github.com/repos/${DOCKER_USER_REPO}/releases/latest
@@ -37,7 +23,7 @@ echo ${BUIILDX_REPO}
 echo ${BUILD_PLATFORM}
 
 
-echo "docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --build-arg CONTAINER_VERSION=${CONTAINER_VERSION:1} --push"
+echo "docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push"
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
@@ -46,7 +32,7 @@ docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
 docker buildx use ${BUIILDX_REPO}
 
 #build multiplatform docker image
-docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --build-arg CONTAINER_VERSION=${CONTAINER_VERSION:1} --push
+docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push
 
 #remove multiplatform environment
 docker buildx rm ${BUIILDX_REPO}
